1a922b67b8ff725501df5eceef5d47e72c5afd0c
singhabhishekcse <singhabhishekcse@yahoo.com>
2021-09-11 11:08:02 +0530

    fetch data form api

--- src/Components/HomeComponent/NewsComponents/NewsIteamComponent.js
+++ src/Components/HomeComponent/NewsComponents/NewsIteamComponent.js
@@ -15,11 +15,11 @@ export class NewsIteamComponent extends Component {
             
              
              <div className="card" style={{width: "18rem"}}>
-                <img src={imgUrl} className="card-img-top" alt="..." />
+                <img src={!imgUrl? "https://www.mca.gov.in/content/dam/mca/images/press_release_image.jpg":imgUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title"> {title}...</h5>
                     <p className="card-text"> {description}...</p>
-                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read more.</a>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary bg-dark text-white">Read more...</a>
                 </div>
                 </div>
                 
